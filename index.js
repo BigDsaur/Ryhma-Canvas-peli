2ecd78c526eb867c319f572b6ba6877486a755b2
Tiipuuba <142894444+Tiipuuba@users.noreply.github.com>
2025-01-21 13:40:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Player from "./player.js"
 import pointsToScreen from "./points.js";
+import Enemy from "./enemy.js"
 import BulletController from "./bulletcontroller.js";
 
 const canvas = document.getElementById('game')
@@ -14,11 +15,12 @@ background.src = "space.png"
 
 const playerBulletController = new BulletController(canvas, 15, "yellow", true);
 const player = new Player(canvas, 12, playerBulletController)
-
+const enemy = new Enemy(canvas, 5)
 
 function game() {
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
     player.draw(ctx)
+    enemy.draw(ctx)
     playerBulletController.draw(ctx)
     pointsToScreen(totalPoints);
 }
