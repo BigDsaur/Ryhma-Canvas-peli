a38008ebb1d3e6fb5fcac53246dc88a14f4edd90
Joona <Joona879@gmail.com>
2025-01-23 10:56:57 +0200

    collision juttuja, cursor bullet ja muuta kivaa

--- enemy.js
+++ enemy.js
@@ -1,39 +1,47 @@
 export default class Enemy {
     constructor(canvas, velocity, bulletController) {
         this.canvas = canvas;
-        this.velocity = velocity
-        this.tempVelocity = velocity
-        this.bulletController = bulletController
+        this.velocity = velocity;
+        this.tempVelocity = velocity;
+        this.bulletController = bulletController;
 
         this.x = this.canvas.width / 2;
         this.y = this.canvas.height / 8;
         this.width = 128;
         this.height = 128;
+        this.health = 5;
     
         this.image = new Image();
-        this.image.src = `enemy.png`;
+        this.image.src = "enemy.png";
     }
-  
+
+    takeDamage() {
+        this.health -= 1;
+        console.log(`Enemy hit! Health: ${this.health}`);
+    }
+
+    isHit(projectile) {
+        return (
+            this.health > 0 &&
+            projectile.x < this.x + this.width &&
+            projectile.x + projectile.width > this.x &&
+            projectile.y < this.y + this.height &&
+            projectile.y + projectile.height > this.y
+        );
+    }
+
+    // yhdistin colliden ja liikumisen
+    move() {
+        this.x += this.velocity;
+
+        if (this.x <= 0 || this.x + this.width >= this.canvas.width) {
+            this.velocity *= -1;
+        }
+    }
+
     draw(ctx) {
         this.bulletController.shoot(this.x + this.width / 2, this.y, -4, 10);
         this.move();
-        this.collideWithWalls();
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
-  
-    move() {
-        this.x += this.velocity;
-    }
-  
-    collideWithWalls() {
-        //left
-        if (this.x < 0) {
-            this.velocity = this.tempVelocity;
-        }
-    
-        //right
-        if (this.x > this.canvas.width - this.width) {
-            this.velocity += -(this.tempVelocity * 2);
-      }
-  }
 }
--- player.js
+++ player.js
@@ -1,77 +1,86 @@
+import Projectile from "./projectile.js";
+
 export default class Player {
-    rightPressed = false
-    leftPressed = false
-    shootPressed = false
-    
+  rightPressed = false;
+  leftPressed = false;
+  shootPressed = false;
+  
   constructor(canvas, velocity, bulletController) {
-    this.canvas = canvas
-    this.velocity = velocity
-    this.bulletController = bulletController
+      this.canvas = canvas;
+      this.velocity = velocity;
+      this.bulletController = bulletController;
 
-    this.x = this.canvas.width / 2
-    this.y = this.canvas.height - 75
-    this.width = 50
-    this.height = 48
-    this.image = new Image()
-    this.image.src = "player.png"
+      this.x = this.canvas.width / 2;
+      this.y = this.canvas.height - 75;
+      this.width = 50;
+      this.height = 48;
+      this.image = new Image();
+      this.image.src = "player.png";
 
-    document.addEventListener("keydown", this.keydown)
-    document.addEventListener("keyup", this.keyup)
+      document.addEventListener("keydown", (event) => this.keydown(event));
+      document.addEventListener("keyup", (event) => this.keyup(event));
   }
 
   draw(ctx) {
-    if (this.shootPressed) {
-      this.bulletController.shoot(this.x + this.width / 2, this.y, 4, 10);
-    }
-    this.move()
-    this.collideWithWalls()
-    ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+      if (this.shootPressed) {
+          this.bulletController.shoot(this.x + this.width / 2, this.y, 4, 10);
+      }
+      this.move();
+      this.collideWithWalls();
+      ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
   }
 
   collideWithWalls() {
-    //left
-    if (this.x < 0) {
-      this.x = 0
-    }
-
-    //right
-    if (this.x > this.canvas.width - this.width) {
-      this.x = this.canvas.width - this.width;
-    }
+      if (this.x < 0) {
+          this.x = 0;
+      }
+      if (this.x > this.canvas.width - this.width) {
+          this.x = this.canvas.width - this.width;
+      }
   }
 
   move() {
-    if (this.rightPressed) {
-      this.x += this.velocity;
-    } else if (this.leftPressed) {
-      this.x += -this.velocity;
-    }
+      if (this.rightPressed) {
+          this.x += this.velocity;
+      } 
+      if (this.leftPressed) {
+          this.x -= this.velocity;
+      }
   }
 
-  keydown = (event) => {
-    if (event.code == "ArrowRight" || event.code == "KeyD") {
-      this.rightPressed = true;
-      console.log("moved")
-    }
-    if (event.code == "ArrowLeft" || event.code == "KeyA") {
-      this.leftPressed = true;
-      console.log("moved")
-    }
-    if (event.code == "Space") {
-      this.shootPressed = true;
-      console.log("shoot")    
-    }
-  };
+  keydown(event) {
+      if (event.code === "ArrowRight" || event.code === "KeyD") {
+          this.rightPressed = true;
+          console.log("Moved right");
+      }
+      if (event.code === "ArrowLeft" || event.code === "KeyA") {
+          this.leftPressed = true;
+          console.log("Moved left");
+      }
+      if (event.code === "Space") {
+          this.shootPressed = true;
+          console.log("Shoot");
+      }
+  }
 
-  keyup = (event) => {
-    if (event.code == "ArrowRight") {
-      this.rightPressed = false;
-    }
-    if (event.code == "ArrowLeft") {
-      this.leftPressed = false;
-    }
-    if (event.code == "Space") {
-      this.shootPressed = false;
-    }
-  };
+  keyup(event) {
+      if (event.code === "ArrowRight" || event.code === "KeyD") {
+          this.rightPressed = false;
+      }
+      if (event.code === "ArrowLeft" || event.code === "KeyA") {
+          this.leftPressed = false;
+      }
+      if (event.code === "Space") {
+          this.shootPressed = false;
+      }
+  }
+
+  shoot(event, canvas) {
+    const rect = canvas.getBoundingClientRect();
+    const mouseX = event.clientX - rect.left;
+    const mouseY = event.clientY - rect.top;
+    return new Projectile(this.x + this.width / 2 - 2.5, this.y, mouseX, mouseY);
 }
+
+}
+
