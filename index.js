1eb8a6d6e09b5ce38c60ddf5e317d95467cd593a
Tiipuuba <142894444+Tiipuuba@users.noreply.github.com>
2025-01-23 10:29:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,38 +6,29 @@ import BulletController from "./bulletcontroller.js";
 const canvas = document.getElementById('game')
 const ctx = canvas.getContext('2d')
 
-const myBackground = new component("background");
+const background = new component()
 
 let totalPoints = 5000
 canvas.width = 800
 canvas.height = 800
 
-function component(type) {
-    this.type = type;
-    if (type == "image" || type == "background") {
-        this.image = new Image();
-        this.image.src = "./space.png";
-    }
+// Moving/Looping background
+function component() {
+    this.image = new Image();
+    this.image.src = "./space.png";
     this.width = 800;
     this.height = 800;
     this.speedY = 1;
     this.x = 0;
     this.y = 0;
+
     this.update = function() {
-        if (type == "image" || type == "background") {
-            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-            if (type == "background") {
-                ctx.drawImage(this.image, this.x, this.y - this.height, this.width, this.height);
-            }
-        }
-    }
-    this.newPos = function() {
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        ctx.drawImage(this.image, this.x, this.y - this.height, this.width, this.height);           
         this.y += this.speedY;
-        if (this.type == "background") {
-            if (this.y >= this.height) {
-                this.y = 0;
-            }
-        }
+        if (this.y >= this.height) {
+            this.y = 0;
+        }  
     }
 }
 
@@ -48,9 +39,7 @@ const player = new Player(canvas, 12, playerBulletController)
 const enemy = new Enemy(canvas, 5, enemyBulletController)
 
 function game() {
-    myBackground.newPos(); 
-    myBackground.update();
-
+    background.update();
     player.draw(ctx)
     enemy.draw(ctx)
     playerBulletController.draw(ctx)
