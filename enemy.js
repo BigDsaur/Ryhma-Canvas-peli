cc947ea13166331c07f54d9d1af14dfb160983f8
Tiipuuba <142894444+Tiipuuba@users.noreply.github.com>
2025-01-27 10:19:34 +0200

    Add files via upload

--- enemy.js
+++ enemy.js
@@ -1,17 +1,15 @@
 export default class Enemy {
-    constructor(canvas, velocity, bulletController, bulletController2, bulletController3) {
+    constructor(canvas, velocity, bulletController) {
         this.canvas = canvas;
         this.velocity = velocity;
         this.tempVelocity = velocity;
         this.bulletController = bulletController;
-        this.bulletController2 = bulletController2;
-        this.bulletController3 = bulletController3;
 
         this.x = this.canvas.width / 2;
         this.y = this.canvas.height / 8;
         this.width = 128;
         this.height = 128;
-        this.health = 25;
+        this.health = 5;
     
         this.image = new Image();
         this.image.src = "./art/enemy.png";
@@ -43,8 +41,6 @@ export default class Enemy {
 
     draw(ctx) {
         this.bulletController.shoot(this.x + this.width / 2, this.y + 100, -4, 4);
-        this.bulletController2.shoot(this.x + 25 + this.width / 2, this.y + 100, -6, 50);
-        this.bulletController3.shoot(this.x - 25 + this.width / 2, this.y + 100, -1.5, 125);
         this.move();
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     }
