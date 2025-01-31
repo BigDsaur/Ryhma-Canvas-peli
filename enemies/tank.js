b6f44e9517318bf875c9f7885edfb0cd10514455
BigDsaur <Joona879@gmail.com>
2025-01-31 11:46:05 +0200

    tankki

---
+++ enemies/tank.js
@@ -0,0 +1,58 @@
+export default class Enemy {
+    constructor(canvas, velocity, bulletController) {
+        this.canvas = canvas;
+        this.velocity = velocity;
+        this.tempVelocity = velocity;
+        this.initialVelocity = velocity;
+        this.bulletController = bulletController;
+
+        this.x = this.canvas.width / 2;
+        this.y = this.canvas.height / 8;
+        this.width = 128;
+        this.height = 128;
+        this.health = 100;
+    
+        this.image = new Image();
+        this.image.src = "./art/dentonator.png";
+    }
+
+    takeDamage(amount) {
+        this.health -= amount;
+        if ( this.health <= 0) {
+            window.location.replace("./index.html");
+        }
+        console.log(`Enemy hit! Health: ${this.health}`);
+    }
+
+    isHit(projectile) {
+        return (
+            this.health > 0 &&
+            projectile.x < this.x + this.width &&
+            projectile.x + projectile.width > this.x &&
+            projectile.y < this.y + this.height &&
+            projectile.y + projectile.height > this.y
+        );
+    }
+
+    move() {
+        this.x += this.velocity;
+    
+        if (this.x <= 0 || this.x + this.width >= this.canvas.width) {
+            this.velocity *= -1;
+        }
+    
+        this.velocity *= 1.001;
+        this.velocity = Math.min(this.velocity, 6);
+
+        if (Math.abs(this.velocity) >= 6) {
+            this.velocity = this.initialVelocity * Math.sign(this.velocity);
+        }
+    }
+
+    draw(ctx, enemyBulletController) {
+        this.bulletController.shoot(this.x + this.width / 2, this.y + 100, -8, 6.5);
+        this.move();
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+        enemyBulletController.draw(ctx)
+    }
+}
